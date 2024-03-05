f4cc6884df799ec6d4febe2a051cab8231b27e45
SergeyB19 <my.h.m@yandex.ru>
2024-03-05 10:59:13 +0300

    Валидация форм. Аннотация @Valid

--- src/main/java/ru/alishev/springcourse/controllers/PeopleController.java
+++ src/main/java/ru/alishev/springcourse/controllers/PeopleController.java
@@ -1,8 +1,10 @@
 package ru.alishev.springcourse.controllers;
 
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import ru.alishev.springcourse.dao.PersonDAO;
 import ru.alishev.springcourse.models.Person;
@@ -37,7 +39,10 @@ public class PeopleController {
     }
 
     @PostMapping()
-    public String create(@ModelAttribute("person") Person person) {
+    public String create(@ModelAttribute("person") @Valid Person person, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return "people/new";
+        }
         personDAO.save(person);
         return "redirect:/people";
     }
@@ -49,7 +54,12 @@ public class PeopleController {
     }
 
     @PatchMapping("/{id")
-    public String update(@ModelAttribute("person") Person person, @PathVariable("id") int id) {
+    public String update(@ModelAttribute("person") @Valid Person person, BindingResult bindingResult,
+                         @PathVariable("id") int id) {
+        if (bindingResult.hasErrors()) {
+            return "people/edit";
+        }
+
         personDAO.update(id, person);
         return "redirect:/people";
     }
